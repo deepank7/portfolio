b3a5626bb7f1cf5a59818075cfa6b8cb28dc44ee
Deepank Sharma <aaronhazard007@gmail.com>
2021-09-05 16:44:46 +0530

    initial commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Deepank Sharma
+      </SectionTitle>
+      <SectionText>
+        Front end developer, experienced in React and Next
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import {
+  DiFirebase,
+  DiGit,
+  DiJavascript,
+  DiReact,
+  DiZend,
+} from "react-icons/di";
+import { SiNextDotJs } from "react-icons/si";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've experience working on multiple stack of technolgies like JavaScript,
+      Java, SQL, Ruby on Rails. My major experience lies in the fron-end
+      technolgies like JS, React, NextJs.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiJavascript size="3rem" />
+        <ListContainer>
+          <ListTitle>JS</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>React</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiNextDotJs size="3rem" />
+        <ListContainer>
+          <ListTitle>Next</ListTitle>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
