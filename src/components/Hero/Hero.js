72602ada369c9e3a8454e85071fe2c47058fdeda
Deepank Sharma <aaronhazard007@gmail.com>
2021-09-11 00:46:15 +0530

    Removed commented code

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,7 +8,6 @@ import {
 import { Typography, Avatar, Grid, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Typed from "react-typed";
-import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
 import { ProfilePicture } from "../../constants/constants";
 
@@ -40,31 +39,6 @@ const Hero = (props) => {
   const classes = useStyles();
   return (
     <Section row nopadding>
-      {/* <Box className={classes.typedContainer}>
-        <Grid container justify="center">
-          <Avatar
-            className={classes.avatar}
-            // src={avatar}
-            alt="Deepank Sharma"
-          />
-        </Grid>
-        <Typography className={classes.title} variant="h4">
-          <Typed strings={["Deepank Sharma"]} typeSpeed={40} />
-        </Typography>
-        <br />
-        <Typography className={classes.subtitle} variant="h5">
-          <Typed
-            strings={[
-              "Front-end Developer",
-              "Web Development",
-              "React Developer",
-            ]}
-            typeSpeed={40}
-            backSpeed={60}
-            loop
-          />
-        </Typography>
-      </Box> */}
       <LeftSection>
         <Box>
         <SectionTitle main center>
